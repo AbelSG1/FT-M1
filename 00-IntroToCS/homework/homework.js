601ef07a54ecbd1e30c5b8cbc511d8bb0fbb74dd
AbelSG1 <lsaavedrag1@gmail.com>
2022-04-25 17:24:44 -0500

    FT-MODULO01

--- 00-IntroToCS/homework/homework.js
+++ 00-IntroToCS/homework/homework.js
@@ -3,11 +3,43 @@
 function BinarioADecimal(num) {
   // tu codigo aca
   // algo
+  let arreglo = num.split("");
+  let suma = 0;
+  for (let i=0; i<arreglo.length; i++ ){
+    suma  = suma + (arreglo[i]) *2 **(arreglo.length-1-i);
+  }
+  return suma;
 }
 
 function DecimalABinario(num) {
   // tu codigo aca
+  var array = [];
+  do{
+   /*  //if(num === 0){
+      //return 0;
+   // }
+    //else if(num === 1){
+    //  array.push(1);
+   // } */
+     if(num % 2 !== 0){
+      array.push(1)
+    }
+    else if(num % 2 === 0){
+      array.push(0)
+    }
+    num = Math.floor(num/2);
+  }
+  while(num >= 1);
+  let ultimoArray = array.reverse().join("")
+  return ultimoArray;
 }
+
+
+// No se pueden usar: 
+// parseInt
+// toString
+
+
 
 module.exports = {
   BinarioADecimal,
