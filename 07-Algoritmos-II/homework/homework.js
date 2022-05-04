ee03436dc655d6d857ca8e901d40846430c25359
AbelSG1 <lsaavedrag1@gmail.com>
2022-05-04 12:08:26 -0500

    quicksort

--- 07-Algoritmos-II/homework/homework.js
+++ 07-Algoritmos-II/homework/homework.js
@@ -6,7 +6,24 @@ function quickSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
-
+  let final = []
+  let derecha = [];
+  let nuevoArray = [];
+  let izquierda = [];
+  let pivot = Math.floor(array.length/2)
+  if (array.length <1)return[];
+  for(let i=0;i<array.length; i++){
+    if(array[i]>array[pivot]){
+      derecha.push(array[i]);
+    }
+    if(array[i]=== array[pivot]){
+      nuevoArray.push(array[i]);
+    }
+    if(array[i]< array[pivot]){
+      izquierda.push(array[i]);
+    }
+  }
+  return final.concat(quickSort(izquierda),nuevoArray, quickSort(derecha));
 }
 
 function mergeSort(array) {
